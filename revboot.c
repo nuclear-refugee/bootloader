dc3497c856141a9ec8b6e4eb058f780ef4506e8f
mickey9910326 <mickey9910326@gmail.com>
2017-09-04 09:54:54 +0800

    remove get_data function in revboot.c

--- revboot.c
+++ revboot.c
@@ -46,29 +46,6 @@ int main(void) {
     }
 }
 
-uint8_t get_data(uint8_t *buf, uint16_t *bytes) {
-    uint8_t res = 0;
-    for (uint8_t i = 0; i < 4; i++)
-        if (prog_getc() != 0xfc)
-            res = 1;
-    if (prog_getc() != 0x01)
-        res = 2;
-    *bytes = prog_getc();
-    *bytes = ((*bytes)<<8) + prog_getc();
-    uint8_t chksum    = 0;
-    for (uint16_t i = 0; i < *bytes; i++) {
-        buf[i] = prog_getc();
-        chksum += buf[i];
-    }
-    for (uint16_t i = *bytes; i < SPM_PAGESIZE; i++) {
-        buf[i] = 0;
-    }
-    uint8_t getchksum = prog_getc();
-    if (getchksum != chksum)
-        res = 3;
-
-    return res;
-}
 void send_OK() {
     prog_putc(0xFC);
     prog_putc(0xFC);
