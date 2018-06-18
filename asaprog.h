5256922b329f5bdda7cef8095f41defd1fcc99a0
mickey9910326 <mickey9910326@gmail.com>
2018-06-18 22:35:22 +0800

    update asaprog packet and optimize code

---
+++ asaprog.h
@@ -0,0 +1,13 @@
+#ifndef __ASAPROG_H
+#define __ASAPROG_H
+
+#include <inttypes.h>
+
+#define MAX_PROGSIZE 4800
+
+uint8_t get_ASA_prog_cmd(uint8_t *buf, uint16_t *bytes);
+uint8_t is_prog_mode(void);
+void put_res_of_start();
+void put_res_of_last();
+
+#endif // __ASAPROG_H
--- revboot.c
+++ revboot.c
@@ -1,67 +1,60 @@
 #include <inttypes.h>
+#include <util\delay.h>
 #include <avr/boot.h>
+#include <avr/wdt.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <stdio.h>
-#include <util\delay.h>
 #include "revprog.h"
-#include "getASAcmd.h"
+#include "asaprog.h"
 
 void (*main_entry_point)(void) = 0x0000;
 
-void send_OK();
-void send_OK2();
-void boot_program_page (uint32_t page, uint8_t *buf);
-
-inline uint8_t bootloader_active() {
-    DDRG = 0;
-    return !(PING&0x01);
-}
+void program_page(uint32_t page, uint8_t *buf);
+void erase_all_flash(void);
 
 int main(void) {
-    prog_init();
 
-    if(bootloader_active()) {
-        uint8_t buf[SPM_PAGESIZE]={1};
+    if(is_prog_mode()) {
+        prog_init();
+        uint8_t buf[SPM_PAGESIZE];
         uint8_t res = 0;
         uint16_t bytes=0;
         uint32_t page = 0;
-        get_ASA_prog_cmd(buf,&bytes);
-        _delay_ms(30);
-        send_OK();
-        res = get_ASA_prog_cmd(buf,&bytes);
-        while(bytes==SPM_PAGESIZE){
-            boot_program_page(page,buf);
-            page += SPM_PAGESIZE;
-            res = get_ASA_prog_cmd(buf,&bytes);
-            _delay_ms(5);
+        uint8_t status = 0;
+
+        while(1) {
+            switch (status) {
+                case 0: // wait for cmd
+                    res = get_ASA_prog_cmd(buf,&bytes);
+                    if(res==1) {  // start asaprog cmd
+                        status = 1;
+                        // erase_all_flash();
+                        put_res_of_start();
+                    }
+                    break;
+                case 1: // wait data cmd and prog
+                    res = get_ASA_prog_cmd(buf,&bytes);
+                    if(res==2) { // data cmd
+                        if(bytes==0) {
+                            status = 0;
+                            put_res_of_last();
+                        } else {
+                            // NOTE is it need to set buf to 0 if the bytes is not full?
+                            program_page(page,buf);
+                            page += SPM_PAGESIZE;
+                        }
+                    } else {
+                        status = 0;
+                    }
+                    break;
+            }
         }
-        if (bytes) {
-            boot_program_page(page,buf);
-        }
-        _delay_ms(30);
-        send_OK();
     } else {
         main_entry_point();
     }
 }
 
-void send_OK() {
-    prog_putc(0xFC);
-    prog_putc(0xFC);
-    prog_putc(0xFC);
-    prog_putc(0xFC);
-    prog_putc(0x01);
-    prog_putc(0x00);
-    prog_putc(0x04);
-    prog_putc('O');
-    prog_putc('K');
-    prog_putc('!');
-    prog_putc('!');
-    prog_putc(0xC4);
-}
-
-void boot_program_page (uint32_t page, uint8_t *buf) {
+void program_page (uint32_t page, uint8_t *buf) {
     uint16_t i;
     uint8_t sreg;
     // Disable interrupts.
@@ -78,8 +71,20 @@ void boot_program_page (uint32_t page, uint8_t *buf) {
 
         boot_page_fill (page + i, w);
     }
-    boot_page_write (page);     // Store buffer in flash page.
+    boot_page_write(page);      // Store buffer in flash page.
     boot_spm_busy_wait();       // Wait until the memory is written.
-    boot_rww_enable ();
+    boot_rww_enable();
     SREG = sreg;
+}
+
+void erase_all_flash() {
+	uint16_t i;
+	uint32_t address = 0;
+	for(i = 0; i < MAX_PROGSIZE; i++) {
+		boot_page_erase(address);
+		boot_spm_busy_wait();
+		boot_rww_enable();
+		address+=SPM_PAGESIZE;
+	}
+	//boot_rww_enable();
 }
